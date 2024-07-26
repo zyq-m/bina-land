e2c3f8ae54f9f802155ffbea52c9969ef219295a
zyq-m <last.game01987@gmail.com>
2024-07-27 02:15:44 +0800

    create search bar comp

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="grid gap-4 grid-flow-row-dense grid-cols-6 px-4 py-6 shadow-md rounded-lg bg-base-100 mb-12">
+      <input
+        type="text"
+        placeholder="Location"
+        className="input input-bordered w-full"
+      />
+      <select className="select select-bordered w-full">
+        <option disabled selected>
+          Price
+        </option>
+        <option>RM0 to RM80,000</option>
+        <option>RM80,000 to RM100,000</option>
+        <option>RM100,000 and above</option>
+      </select>
+      <select className="select select-bordered w-full">
+        <option disabled selected>
+          Property Type
+        </option>
+        <option>Land</option>
+        <option>Commercial Real Estate</option>
+        <option>Industrial Real Estate</option>
+        <option>Residential Real Estate</option>
+      </select>
+      <input
+        type="number"
+        placeholder="Size (square feet unit)"
+        className="input input-bordered w-full"
+      />
+      <div className="flex col-span-2 gap-1">
+        <input
+          type="search"
+          placeholder="Search..."
+          className="input input-bordered w-full"
+        />
+        <button type="submit" className="px-6 btn bg-[#FF5A3C] text-white ">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Layout from "./Layout";
 import PropertyCard from "./PropertyCard";
+import SearchBar from "./SearchBar";
 
-export { Layout, PropertyCard };
+export { Layout, PropertyCard, SearchBar };
