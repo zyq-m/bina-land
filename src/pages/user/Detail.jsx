afe2da5240465e48cb3ec0c90277b4b8c6fa09f0
muhdfauzan09 <muhdfauzan114@gmail.com>
2024-08-21 22:42:35 +0800

    update code

--- src/pages/user/Detail.jsx
+++ src/pages/user/Detail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react
+import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { useParams } from "react-router-dom";
